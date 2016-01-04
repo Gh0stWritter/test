d52e2b72561341834909c515ee7f4d6076de0f29
Gh0stWritter <fear1911@gmail.com>
2016-01-05 00:31:27 +1000

    test
    test

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,38 @@
+// ConsoleApplication2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void functionFirst()
+{
+	string detector = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq";
+
+	int nameObj[1];
+
+	for (int j = 0;; j++)
+		if (nameObj[j] == 1)
+			break;
+}
+
+void functionSecond()
+{
+	string detector = "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww";
+
+	int i = 0;
+	int nameObj[1];
+
+	for (int j = 0; i == 1; j++)
+		if (nameObj[j] == 1)
+			i = 1;
+}
+
+int main()
+{
+	cout << "Hello World!" << endl;
+
+	return 0;
+}
